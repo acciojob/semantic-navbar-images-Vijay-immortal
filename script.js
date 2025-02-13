c08e0afc5abeb6d34996d24868298576ca033e2b
Vijay-immortal <103632109+Vijay-immortal@users.noreply.github.com>
2025-02-13 10:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+cy.get('img').should('be.visible').should($imgs => {
+  expect($imgs).to.have.length(3);
+  const srcs = $imgs.map((i, el) => Cypress.$(el).attr('src'));
+  expect(srcs.get()).to.deep.eq([
+    "https://picsum.photos/id/123/200/300",
+    "https://picsum.photos/id/124/200/300",
+    "https://picsum.photos/id/125/200/300"
+  ]);
+});
+cy.wait(2000); // Adjust time as needed
+cy.get('img').should('have.length', 3).each(($img) => {
+  cy.wrap($img).should('have.attr', 'src');
+});
