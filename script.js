feccf702568b1f58dd6b907c3dd825150c32b47c
Vijay-immortal <103632109+Vijay-immortal@users.noreply.github.com>
2025-02-25 17:41:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-//your code here
-cy.get('img').should('be.visible').should($imgs => {
-  expect($imgs).to.have.length(3);
-  const srcs = $imgs.map((i, el) => Cypress.$(el).attr('src'));
-  expect(srcs.get()).to.deep.eq([
-    "https://picsum.photos/id/123/200/300",
-    "https://picsum.photos/id/124/200/300",
-    "https://picsum.photos/id/125/200/300"
-  ]);
-});
-cy.wait(2000); // Adjust time as needed
-cy.get('img').should('have.length', 3).each(($img) => {
-  cy.wrap($img).should('have.attr', 'src');
+describe("Example Image Test", () => {
+    beforeEach(() => {
+        cy.visit("/main.html"); // Ensure baseUrl is correctly set in cypress.config.js
+    });
+
+    it("3 images with heading are there", () => {
+        cy.contains("3 random images");
+
+        cy.get("img").should($imgs => {
+            expect($imgs).to.have.length(3);
+            const srcs = $imgs.map((i, el) => Cypress.$(el).attr("src"));
+            expect(srcs.get()).to.deep.eq([
+                "https://picsum.photos/id/123/200/300",
+                "https://picsum.photos/id/124/200/300",
+                "https://picsum.photos/id/125/200/300"
+            ]);
+        });
+    });
 });
